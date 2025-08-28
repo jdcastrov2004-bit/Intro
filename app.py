75ec18c229f1cce52e9d9bfc7c42a42a04fa792d
jdcastrov2004-bit <jdcastrov2004@gmail.com>
2025-08-28 12:49:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Mi primera app")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces")
-st.write("Facilmente úedo realizar back-end y front-end")
+st.write("Facilmente puedo realizar back-end y front-end")
 image = Image.open('Travis meme.png')
 
 st.image(image, caption='interfaces multimodales')
