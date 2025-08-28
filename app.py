ae320372a3cea6c3f5390b9f95eab922af58b231
jdcastrov2004-bit <jdcastrov2004@gmail.com>
2025-08-28 12:48:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Mi primera app")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces")
+st.write("Facilmente úedo realizar back-end y front-end")
+image = Image.open('Travis meme.png')
+
+st.image(image, caption='interfaces multimodales')
+
