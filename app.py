e394cd2e5c25b7ff1ac834ecbe6e24ee26eb1a34
jdcastrov2004-bit <jdcastrov2004@gmail.com>
2025-10-21 20:23:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ st.title("🔥 Mi primera app interactiva 🔥")
 st.header("Aquí empieza mi aventura con las interfaces multimodales 😎")
 
 st.write(
-    "En este espacio empiezo a crear mis propias apps, mezclando diseño, lógica y un poco de flow. "
-    "Aquí puedo conectar el **back-end** con el **front-end** sin complicarme demasiado."
+    "En este espacio empiezo a crear mis propias apps, mezclando diseño y un poco de flow. "
+    "Aquí puedo conectar el **back-end** con el **front-end** (Todavía no se muy bien que son) sin complicarme demasiado."
 )
 
 image = Image.open('Travis meme.png')
 st.image(image, caption='Un toque de humor para las interfaces 😄')
 
-texto = st.text_input('💬 Escribe algo aquí:', '¡Hola mundo, soy mi propia interfaz!')
+texto = st.text_input('💬 Escribe algo aquí:', '¡Hola mundo! jajajaja')
 st.write('🪄 Tu texto dice:', texto)
-st.subheader("✨ Ahora juguemos con dos columnas y sigamos explorando ✨")
+st.subheader("✨ Ahora juguemos con dos columnas")
